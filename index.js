796f274207f5eb7756d7c361deb18684083b3d3c
Terence Holmes <piece4ulmind@gmail.com>
2022-03-27 00:27:01 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(target = "roller-skate") {
+    return `This Saturday, I want to ${target}!`;
+}
+saturdayFun(target);
+saturdayFun();
+
+function mondayWork(target = "go to the office") {
+    return `This Monday, I will ${target}.`;
+ }
+ mondayWork(target);
+ mondayWork();
+ 
+const symbol = "*"; 
+function wrapAdjective(symbol) {
+   const innerWrap = function (adj = "special") {
+   return `You are ${symbol}${adj}${symbol}!`;
+   };
+   return innerWrap;
+}
+wrapAdjective("*");
